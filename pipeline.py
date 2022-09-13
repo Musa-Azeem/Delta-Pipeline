6cdbd0c00274edb23e1fb6dc2858122322941511
MMAZeem3 <musa.mazeem@gmail.com>
2022-09-13 13:39:35 -0400

    pipeline

---
+++ pipeline.py
@@ -0,0 +1,58 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from datetime import timedelta
+
+
+if len(sys.argv) != 2:
+    print("Usage: python3 timedeltas.py [path-to-data-directory]")
+    exit(1)
+
+os.system('mkdir -p processed')
+
+dir = sys.argv[1]
+
+df = pd.DataFrame()
+for file in os.listdir(dir):
+    if file.startswith('Session'):
+        filename = file.split('.')[1]
+        with open(f'{dir}/{file}') as f:
+            starttime_millis = int(f.readline().rstrip()[17:])
+        session = pd.read_csv(f'{dir}/{file}', skiprows=1)
+    else:
+        tmp = pd.read_csv(f'{dir}/{file}', skiprows=1)
+        df = pd.concat([df, tmp], ignore_index=True)
+df = df.sort_values(by=['timestamp'], ignore_index=True)
+
+print(len(df))
+
+# get readable time (delta from start time)
+real_times = []
+for time_millis in df['real time']:
+    delta = (time_millis - starttime_millis) # convert to seconds
+    real_times.append(str(timedelta(milliseconds=delta)))
+df['human time'] = real_times
+
+# get time delta at each sample
+deltas = []
+for i, timestamp in enumerate(df['timestamp']):
+    if i == 0:
+        deltas.append(np.nan)
+        continue
+    # get time since last sample, convert to Hz
+    deltas.append( (df['timestamp'][i] - df['timestamp'][i-1]) / 1e9)
+df['delta'] = deltas
+
+print(df['delta'].describe())
+print(df)
+
+df.to_csv(f'processed/{filename}-processed.csv')
+
+fig = px.line(df, x='human time', y=['acc_x', 'acc_y', 'acc_z'], 
+            #   color='activity',
+              title='Accelerometer Data',
+              labels={'human time': 'Time', 'variable': 'Legend'})
+fig.update_layout(yaxis_title="Acceleration (m/s^2)")
+fig.show(renderer='browser')
