a6b84d5dc012df7acb011ed50ff739452fde23cf
MMAZeem3 <musa.mazeem@gmail.com>
2022-09-13 15:04:57 -0400

    switch x axis

--- timedeltas.py
+++ timedeltas.py
@@ -50,7 +50,7 @@ print(df)
 
 df.to_csv(f'processed/{filename}-processed.csv')
 
-fig = px.line(df, x='human time', y='delta', title='Time delta', 
-                labels={'human time': 'Time', 
+fig = px.line(df, x='timestamp', y='delta', title='Time delta', 
+                labels={'timestamp': 'Time', 
                         'delta': 'Time delta (s)'})
 fig.show(renderer='browser')
